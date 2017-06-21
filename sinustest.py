bb2c00c68318c6214549cb5bac6bc9c5f7614ab8
Lukas Steffen <lukas@debian-develop.steffen-steffen.ch>
2017-06-21 17:34:29 +0200

    bugfix sinustest.py to scale with y

--- sinustest.py
+++ sinustest.py
@@ -7,7 +7,7 @@ def sinus_line_points(startx, starty, endx, endy, step=0.25):
     npoints = int((endx - startx)/step) + 1 # number of points to calculate
     xgses = [ i*step for i in range(npoints) ] # start and end to be removed
     # ystep = (endy-starty) / npoints
-    # yies = [ i*ystep for i in ]
+    yies = [ ]
     for x in xgses:
         yies.append((endy-starty) * (x - sin(radians(x*360))*(step/sin(radians(step*360)))))
     return lists_to_tuples([x + startx for x in xgses ], [y + starty for y in yies])
@@ -20,3 +20,4 @@ def lists_to_tuples(list1, list2):
         return result
 
 if __name__=="__main__":
+    print sinus_line_points(1,1,2,5,0.1)
