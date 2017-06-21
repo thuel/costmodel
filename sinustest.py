5cadeb8f1bdf1cb2526d94b0f73ce5b7a55a2304
Lukas Steffen <lukas@steffen-steffen.ch>
2017-06-21 17:29:23 +0200

    add sinustest & amend sinus function
    Try to scale y-values to intervall greater than 1.

--- sinustest.py
+++ sinustest.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-
-"""Main cost model script
-"""
-
-from pltgrph import *
-from netwrkgrph import *
 from math import radians, sin
 
 def sinus_line_points(startx, starty, endx, endy, step=0.25):
@@ -26,16 +20,3 @@ def lists_to_tuples(list1, list2):
         return result
 
 if __name__=="__main__":
-    graph=Graph()
-    graph.add_edge(2,5,1,1, sinus_line_points(1,1,2,0.2))
-    graph.add_edge(2,5,7,5)
-    graph.add_edge(2,5,3,6)
-    graph.add_edge(3,3,1,1, sinus_line_points(1,1,3,0.1)) 
-    graph.add_edge(3,6,7,5)
-    graph.add_edge(0,0,1,1,[(0.2,0.375),(0.375,0.625),(0.7,0.875)])
-    graph.add_edge(0,2,1,1)
-    graph.add_edge(1,1,6,1)
-    graph.add_edge(3,3,4,2)
-    calc_neighbours(graph.vertices, graph.edges)
-
-    print_graph(graph)
