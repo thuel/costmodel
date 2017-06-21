cb11bc13c4d3be49eeb804466e9f6f2e89e6c702
thuel <thuel@me.com>
2017-06-21 16:59:31 +0200

    Add sinus curve function
    Doesn't work as expected if the slope is not 1.

--- main_model.py
+++ main_model.py
@@ -5,25 +5,35 @@
 
 from pltgrph import *
 from netwrkgrph import *
+from math import radians, sin
 
-graph=Graph()
-graph.add_edge(2,5,1,1)
-graph.add_edge(2,5,7,5)
-graph.add_edge(2,5,3,6)
-graph.add_edge(3,3,1,1) #try to add list with sinus wave
-"""sinus wave:
-startx, step, endx as givens normed to 0
-xgses = [ i*step for i in range(int((endx - startx)/step) + 1) ]
-yies = []
-for x in xgses:
-    yies.append(x - sin(x*step*360)*(step/sin(step*360)))
+def sinus_line_points(startx, starty, endx, step=0.25):
+    """sinus line points calculation:
+    """
+    xgses = [ i*step for i in range(int((endx - startx)/step) + 1) ]
+    yies = []
+    for x in xgses:
+        yies.append(x - sin(radians(x*360))*(step/sin(radians(step*360))))
+    return lists_to_tuples([x + startx for x in xgses ], [y + starty for y in yies])
 
-"""
-graph.add_edge(3,6,7,5)
-graph.add_edge(0,0,1,1,[(0.2,0.375),(0.375,0.625),(0.7,0.875)])
-graph.add_edge(0,2,1,1)
-graph.add_edge(1,1,6,1)
-graph.add_edge(3,3,4,2)
-calc_neighbours(graph.vertices, graph.edges)
+def lists_to_tuples(list1, list2):
+    if len(list1) == len(list2):
+        result = []
+        for i in range(len(list1)):
+            result.append((list1[i], list2[i]))
+        return result
 
-print_graph(graph)
+if __name__=="__main__":
+    graph=Graph()
+    graph.add_edge(2,5,1,1, sinus_line_points(1,1,2,0.2))
+    graph.add_edge(2,5,7,5)
+    graph.add_edge(2,5,3,6)
+    graph.add_edge(3,3,1,1, sinus_line_points(1,1,3,0.1)) 
+    graph.add_edge(3,6,7,5)
+    graph.add_edge(0,0,1,1,[(0.2,0.375),(0.375,0.625),(0.7,0.875)])
+    graph.add_edge(0,2,1,1)
+    graph.add_edge(1,1,6,1)
+    graph.add_edge(3,3,4,2)
+    calc_neighbours(graph.vertices, graph.edges)
+
+    print_graph(graph)
