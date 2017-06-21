5eae9eef3ecb32311fc079310a4981dac181f0b0
thuel <thuel@me.com>
2017-06-21 14:59:41 +0200

    add sinus curve calculation

--- main_model.py
+++ main_model.py
@@ -10,9 +10,17 @@ graph=Graph()
 graph.add_edge(2,5,1,1)
 graph.add_edge(2,5,7,5)
 graph.add_edge(2,5,3,6)
-graph.add_edge(3,3,1,1)
+graph.add_edge(3,3,1,1) #try to add list with sinus wave
+"""sinus wave:
+startx, step, endx as givens normed to 0
+xgses = [ i*step for i in range(int((endx - startx)/step) + 1) ]
+yies = []
+for x in xgses:
+    yies.append(x - sin(x*step*360)*(step/sin(step*360)))
+
+"""
 graph.add_edge(3,6,7,5)
-graph.add_edge(0,0,1,1,[(0.25,0.3),(0.5,0.8),(0.75,0.8)])
+graph.add_edge(0,0,1,1,[(0.2,0.375),(0.375,0.625),(0.7,0.875)])
 graph.add_edge(0,2,1,1)
 graph.add_edge(1,1,6,1)
 graph.add_edge(3,3,4,2)
